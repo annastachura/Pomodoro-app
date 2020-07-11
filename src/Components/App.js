56fae2384b5b1581479342c1d9f23474e6637679
Ania <ania@Macieks-MacBook-Pro.local>
2020-07-11 16:35:03 +0200

    Add all components to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,8 @@
 import react from "react";
+import Header from "../Components/Header";
+import Buttons from "../Components/Buttons";
+import PomodoroTime from "../Components/PomodoroTime";
+
 
 
 class App extends React.Component {
@@ -6,11 +10,16 @@ class App extends React.Component {
         super(props);
     this.state  = {
 
-
     }
 }
     render () {
-        
+        return (
+            <div>
+            <Header></Header>
+            <PomodoroTime></PomodoroTime>
+            <Buttons></Buttons>
+            </div>
 
+        )
     }
 }
