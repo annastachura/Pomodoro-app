efbdd02aac6097df2135555e824667dcfced9e25
Ania <ania@Macieks-MacBook-Pro.local>
2020-07-12 22:14:18 +0200

    Add some buttons scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
-import "./styles/main.sasss";
+import "./styles/main.scss";
 
 
 ReactDOM.render(<App/>, document.getElementById("root"));
