92b630f66cf7aeea3dc7e9bf79f0688337924e10
Ania <ania@Macieks-MacBook-Pro.local>
2020-07-12 21:46:29 +0200

    Add render App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import React from "react";
-import uuid from "uuid";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import "./styles/main.sasss";
 
+
+ReactDOM.render(<App/>, document.getElementById("root"));
